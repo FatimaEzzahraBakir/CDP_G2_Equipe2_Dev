e36a91c4f4b403cba18fdad56fcd2760bad19b77
Claire Marciniak <15686831+clairrouf@users.noreply.github.com>
2019-10-29 11:49:55 +0100

    Création page d'accueil

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+var path = require('path')
+
+
+// set the view engine to ejs
+app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/src/views'))
+app.use(express.static(path.join(__dirname , 'public')));
+
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.listen(8080);
