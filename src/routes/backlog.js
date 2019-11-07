8a86bfe987d20a29c95b76e383634ded15cae381
Claire Marciniak <15686831+clairrouf@users.noreply.github.com>
2019-11-07 22:26:45 +0100

    page modification issue + action modification

--- src/routes/backlog.js
+++ src/routes/backlog.js
@@ -25,7 +25,7 @@ module.exports = function(app){
           resolve(res);
         }
         if(project.issues.length == 0)
-          resolve(res);
+        resolve(res);
         project.issues.forEach(function(issue_id, i){
           Issue.findById(issue_id).exec().then(function(issue){
             res.push(issue);
@@ -40,29 +40,72 @@ module.exports = function(app){
       });
     });
 
-    app.get('/user/:login/projects/:name/backlog/:id/delete', async function(req, res){
-      Issue.findOneAndDelete({_id: req.params.id}, function (err) {
+    app.get('/user/:login/projects/:name/backlog/:id/delete', function(req, res){
+      Issue.findOneAndDelete({_id: req.params.id}, async function (err) {
         if(err) console.log(err);
-      });
-
-      let userProjects = await User.find({login: req.params.login});
-      let project;
-      let projectPromise = new Promise(function(resolve, reject){
-        userProjects[0].projects.forEach(async function(p){
-          let tmp = await Project.findById(p);
-          if(tmp.name == req.params.name)
-          resolve(tmp);
-        })
-      }).then(function(project){
-        project.issues.forEach(function(issue){
-          //à revoir le delete d'un issue dans un projet
-          Issue.findOneAndDelete({_id: req.params.id}, function (err) {
-            if(err) console.log(err);
+        let userProjects = await User.find({login: req.params.login});
+        let project;
+        //On recupere le projet
+        let projectPromise = new Promise(function(resolve, reject){
+          userProjects[0].projects.forEach(async function(p){
+            let tmp = await Project.findById(p);
+            if(tmp.name == req.params.name)
+            resolve(tmp);
+          })
+        }).then(function(project){
+          if (err) throw err;
+          //On recupere les issues du projet
+          let issues = new Promise(function(resolve) {
+            let res = [];
+            if(typeof project.issues == 'undefined'){
+              resolve(res);
+            }
+            if(project.issues.length == 0)
+            resolve(res);
+            project.issues.forEach(function(issue_id, i){
+              Issue.findById(issue_id).exec().then(function(issue){
+                res.push(issue);
+                if(i === (project.issues.length - 1)) resolve(res);
+              });
+            });
+          }).then(function(issues){
+            console.log(project.issues + " et " + req.params.id);
+            //ne marche pas encore : ne fait rien
+            Project.updateMany(
+              {_id: {$in: project.issues}}, {$pull: {issues: req.params.id}}, function(err, result){
+                return res.render('backlog', {user: req.user.login, project: project, issues: issues});
+              });
+            })
           });
-        })
-        return res.render('backlog', {user: req.user.login, project: project, issues: []});
-      });
-
+        });
     });
+
+    app.get('/user/:login/projects/:name/backlog/:id/update', async function(req, res) {
+      if(typeof req.user == 'undefined' || req.params.login !== req.user.login)
+      return res.send('Accès non autorisé');
+      let issue = await Issue.findById(req.params.id);
+      return res.render('updateIssue', {user: req.user.login, projectName: req.params.name,issue: issue});
+    });
+
+    app.post('/user/:login/projects/:name/backlog/:id/update', async function(req, res) {
+      if(typeof req.user == 'undefined' || req.params.login !== req.user.login)
+      return res.send('Accès non autorisé');
+
+      Issue.findOneAndUpdate({
+        _id: req.params.id,
+      },
+        {
+          description: req.body.description,
+          difficulty: req.body.difficulty,
+          state: req.body.state,
+          priority: req.body.priority
+        },
+        function (err, project) {
+          if (err) throw err;
+          res.redirect('/user/' + req.params.login + '/projects/' + req.params.name + '/backlog');
+        });
+      
+    });
+
 
   }
