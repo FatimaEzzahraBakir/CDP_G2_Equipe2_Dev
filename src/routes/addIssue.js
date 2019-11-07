c2ed2c8996b7434a77d9d9749da34d2057a3c5e8
FatimaEzzahraBakir <fatimaezzahra.bakir@gmail.com>
2019-11-08 00:13:23 +0100

    debut addIssue back

--- src/routes/addIssue.js
+++ src/routes/addIssue.js
@@ -1,14 +1,29 @@
 const Issue = require('../mongoose/model/issue.model');
 const User = require('../mongoose/model/project.model');
 const Task = require('../mongoose/model/task.model');
+const check  = require('express-validator');
 
 module.exports = function (app) {
     app.get('/user/:login/projects/:name/addIssue', function(req, res) {
         res.render('addIssue',{ userLogin: req.params.login, projectName: req.params.name, error: req.flash("error") });
       });
 
-    app.post('', function(req, res){
-             
-        res.render('addIssue');
-    });
-}
+    app.post('/user/:login/projects/:name/addIssue',(req, res) => {
+           let  IssueInstance = new Issue({
+                description: req.body.description,
+                priority: req.body.priority,
+                difficulty: req.body.difficulty,
+                state: req.body.state,
+                projects: [],
+                tasks :[]
+              })
+            
+              IssueInstance.save()
+                .then(item => {
+                    res.redirect('/user/' + req.params.login + '/projects/' + project.name+'/backlog');
+                })
+                .catch(err => {
+                    res.redirect('/user/' + req.params.login + '/projects/' + project.name+'/backlog');
+                });
+            });
+        }
