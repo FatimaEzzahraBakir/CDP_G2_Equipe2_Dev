c65ea7f6ff8c7b5a7a9345960575134e4c85045d
FatimaEzzahraBakir <fatimaezzahra.bakir@gmail.com>
2019-11-08 14:49:02 +0100

    ajout issue back

--- src/routes/addIssue.js
+++ src/routes/addIssue.js
@@ -1,6 +1,6 @@
 const Issue = require('../mongoose/model/issue.model');
 const User = require('../mongoose/model/project.model');
-const Task = require('../mongoose/model/task.model');
+const Project = require('../mongoose/model/project.model');
 const check  = require('express-validator');
 
 module.exports = function (app) {
@@ -8,22 +8,37 @@ module.exports = function (app) {
         res.render('addIssue',{ userLogin: req.params.login, projectName: req.params.name, error: req.flash("error") });
       });
 
-    app.post('/user/:login/projects/:name/addIssue',(req, res) => {
-           let  IssueInstance = new Issue({
-                description: req.body.description,
-                priority: req.body.priority,
-                difficulty: req.body.difficulty,
-                state: req.body.state,
-                projects: [],
-                tasks :[]
-              })
-            
-              IssueInstance.save()
-                .then(item => {
-                    res.redirect('/user/' + req.params.login + '/projects/' + project.name+'/backlog');
-                })
-                .catch(err => {
-                    res.redirect('/user/' + req.params.login + '/projects/' + project.name+'/backlog');
-                });
+    app.post('/user/:login/projects/:name/addIssue',[
+        check('description').not().isEmpty(),
+        check('difficulty').not().isEmpty(),
+        check('state').not().isEmpty(),
+        check('priority').not().isEmpty()
+      ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.render('addIssue', {
+            errors: errors.array()
+          });
+        }
+    
+        let IssueInstance = new Issue({
+          description: req.body.description,
+          priority: req.body.priority,
+          difficulty: req.body.difficulty,
+          state: req.body.state,
+          projects: [],
+          tasks:[]
+        })
+    
+        IssueInstance.save(function (err, issue) {
+          if (err) throw err;
+          issue.findOneAndUpdate(
+            { login: req.params.login },
+            { $push: { issues: issue.id } },
+            function (err, user) {
+              if (err) throw err;
+              res.redirect('/user/' + req.params.login + '/projects/' + project.name+'/backlog');
             });
+        });
+      });
         }
