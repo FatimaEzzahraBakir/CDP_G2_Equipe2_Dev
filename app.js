402cae52e4f2c1a001b865d8488ba1ffb534acb1
Cyril <cyril.caulonque@protonmail.com>
2019-11-16 16:00:37 +0100

    tri du tableau des issues

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
 const flash = require('connect-flash');
-const path = require('path')
+const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
@@ -31,17 +31,17 @@ app.use(passport.session());
 // set the view engine to ejs
 app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/src/views'))
+app.set('views', path.join(__dirname, '/src/views'));
 app.use(express.static(path.join(__dirname , 'public')));
 
-require('./src/routes/index')(app)
-require('./src/routes/signin')(app)
-require('./src/routes/signup')(app)
-require('./src/routes/logout')(app)
-require('./src/routes/users')(app)
-require('./src/routes/project')(app)
-require('./src/routes/backlog')(app)
-require('./src/routes/addIssue')(app)
-require('./src/routes/sprints')(app)
+require('./src/routes/index')(app);
+require('./src/routes/signin')(app);
+require('./src/routes/signup')(app);
+require('./src/routes/logout')(app);
+require('./src/routes/users')(app);
+require('./src/routes/project')(app);
+require('./src/routes/backlog')(app);
+require('./src/routes/addIssue')(app);
+require('./src/routes/sprints')(app);
 
 app.listen(8080);
